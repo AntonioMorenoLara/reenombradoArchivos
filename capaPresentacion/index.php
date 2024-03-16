d5cd1cb9c2408e805e2dd7f9ed98ee2ba1654311
AntonioMorenoLara <toni.ml7252@gmail.com>
2024-03-16 09:44:32 +0100

    comienzo website

---
+++ capaPresentacion/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+        <img src="./images/logo.jpg" alt="logoArchivo">
+        <h1>Reenombrado de archivos</h1>
+        <main>
+            <article>
+                <h2>¿Qué quieres cambiar?</h2>
+                <section></section>
+                <section></section>
+            </article>
+        </main>
+    </header>
+</body>
+</html>
