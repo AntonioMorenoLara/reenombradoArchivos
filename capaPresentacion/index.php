1dec1ffe697d39d93267cc8e556ce385c271f8cf
AntonioMorenoLara <toni.ml7252@gmail.com>
2024-04-20 09:49:30 +0200

    vista de la pagina index

--- capaPresentacion/index.php
+++ capaPresentacion/index.php
@@ -4,18 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link href="css/estilos.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=family=Montserrat:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <img src="./images/logo.jpg" alt="logoArchivo">
-        <h1>Reenombrado de archivos</h1>
-        <main>
-            <article>
-                <h2>¿Qué quieres cambiar?</h2>
-                <section></section>
-                <section></section>
-            </article>
-        </main>
+    <header class="cabeceraIndex">
+        <img class="logo" src="./images/logo.jpg" alt="logoArchivo">
+        <h1 class="tituloIndex">Reenombrado de archivos</h1>
     </header>
+    <main>
+        <article class="articleIndex">
+            <h2 class="subtituloIndex">¿Qué quieres cambiar?</h2>
+            <section>
+                <a href="unArchivo.php">
+                    <button class="boton">Un archivo</button>
+                </a>
+            </section>
+            <section>
+                <a href="variosArchvios.php">
+                    <button class="boton">Varios Archivos</button>
+                </a>
+            </section>
+        </article>
+    </main>
+    
 </body>
 </html>
